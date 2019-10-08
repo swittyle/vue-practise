a7081e860e66822652b9f66085af5b246f9afda0
shendadi <675834325@qq.com>
2019-10-08 20:44:31 +0800

    vuex table selec

--- src/store.js
+++ src/store.js
@@ -1,16 +1,31 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    tableData:[]
   },
   mutations: {
-
+    setData(state,payload){
+      console.log(state,payload)
+      state.tableData=payload
+    }
+  },
+  getters:{
+    getData(state){
+      return state.tableData
+    },
   },
   actions: {
-
+    getData(context,num){
+      console.log(num)
+      axios.get(`https://randomuser.me/api?results=${num}`)
+        .then(function (response) {
+          context.commit('setData',response.data.results)
+        })
+    }
   }
 })
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,92 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+  <div class="nav-bar">
+    <el-select v-model="value" placeholder="请选择" @change="handleCheckedCitiesChange"
+    >
+      <el-option
+        v-for="item in options"
+        :key="item.value"
+        :label="item.label"
+        :value="item.value"
+      >
+      </el-option>
+    </el-select>
+  </div>
+  <el-table
+    :data="getData"
+    class="table-demo"
+  >
+    <el-table-column
+      prop="gender"
+      label="日期"
+    >
+    </el-table-column>
+    <el-table-column
+      prop="nat"
+      label="姓名"
+    >
+    </el-table-column>
+    <el-table-column
+      prop="email"
+      label="地址"
+    >
+    </el-table-column>
+  </el-table>
   </div>
 </template>
+
+<script>
+  import { mapGetters } from 'vuex'
+
+
+  export default {
+
+    data () {
+      console.log(this.$store,555)
+      return {
+        options:[{
+          value: '1',
+          label: '1'
+        }, {
+          value: '2',
+          label: '2'
+        }, {
+          value: '3',
+          label: '3'
+        }, {
+          value: '4',
+          label: '4'
+        }, {
+          value: '5',
+          label: '5'
+        }],
+        value: ''
+      }
+    },
+
+    computed: {
+      // getData(state){
+      //   return state.tableData
+      // },
+      ...mapGetters(['getData'])
+    },
+
+    mounted () {
+      this.$store.dispatch('getData')
+
+    },
+
+    methods:{
+      handleCheckedCitiesChange (value) {
+        console.log(value)
+        this.$store.dispatch('getData',value)
+      }
+    }
+  }
+</script>
+
+<style>
+  .table-demo {
+    width: 800px;
+  }
+</style>
